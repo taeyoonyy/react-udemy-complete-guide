eff03eebd98414d8ab22828b0c7dd068622a1c03
taeyoonyy <tyyu@lsis.com>
2022-02-06 18:59:18 +0900

    267. 액션 생성자 Thunk 사용하기

--- 19-redux-high-level/src/App.js
+++ 19-redux-high-level/src/App.js
@@ -3,8 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from './store/cart-slice'
 
 let isInitial = true;
 
@@ -18,48 +18,11 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-7088d-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success...",
-          message: "Sent cart data successfully",
-        })
-      );
-    };
-    console.log('where1')
     if (isInitial) {
-      console.log('where2?')
       isInitial = false;
       return;
     }
-    sendCartData().catch((error) =>
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sent cart data failed",
-        })
-      )
-    );
-    // const responseData = await response.json();
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <Fragment>
