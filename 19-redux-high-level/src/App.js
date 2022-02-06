b250facd3fae31c13c5daf769bc0367c3ded2b96
taeyoonyy <tyyu@lsis.com>
2022-02-06 18:38:53 +0900

    266. 리덕스로 Http State 및 피드백 처리하기

--- 19-redux-high-level/src/App.js
+++ 19-redux-high-level/src/App.js
@@ -1,15 +1,80 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible)
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  // useSelector 로 리덕스의 데이터를 보고 있으니, 리덕스 스토어가 바뀔때 마다 아래 컴포넌트 함수가 재실행 되고 최신의 상태를 얻을 수 있다.
+  // useEffect는 비동기를 걸면 안된다. (async, await)
+  // 별개의 함수에서 wrap up해야한다.
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-7088d-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success...",
+          message: "Sent cart data successfully",
+        })
+      );
+    };
+    console.log('where1')
+    if (isInitial) {
+      console.log('where2?')
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) =>
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sent cart data failed",
+        })
+      )
+    );
+    // const responseData = await response.json();
+  }, [cart, dispatch]);
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
