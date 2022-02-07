010bd5dd7850efb6f36705ce339989616c66f4e7
taeyoonyy <tyyu@lsis.com>
2022-02-08 00:14:48 +0900

    283. 사용자 리다이렉션

--- 20-router/src/pages/Welcome.js
+++ 20-router/src/pages/Welcome.js
@@ -1,6 +1,15 @@
-const Welcome = () => {
-  return <h1>The welcom page</h1>;
+import { Route } from 'react-router-dom';
 
+const Welcome = () => {
+  return (
+    <section>
+      <h1>The welcom page</h1>
+      <Route path="/welcome/new-user">
+        <p>Wellcome new user</p>
+      </Route>
+    </section>
+
+  )
 }
 
 export default Welcome;
