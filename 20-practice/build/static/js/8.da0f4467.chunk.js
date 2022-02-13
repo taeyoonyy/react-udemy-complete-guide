6c3825cbf3abe1d2d0fade1646fbdf3dfcbc9647
taeyoonyy <tyyu@lsis.com>
2022-02-13 21:45:39 +0900

    299~305. Section 21. 리액트 앱 배포하기 완료

---
+++ 20-practice/build/static/js/8.da0f4467.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 8.da0f4467.chunk.js.LICENSE.txt */
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[8],{50:function(e,t,c){"use strict"}}]);
+//# sourceMappingURL=8.da0f4467.chunk.js.map
--- 20-practice/src/App.js
+++ 20-practice/src/App.js
@@ -1,31 +1,41 @@
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import AllQuote from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
+// import AllQuote from "./pages/AllQuotes";
+// import QuoteDetail from "./pages/QuoteDetail";
+// import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/ui/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
 
 function App() {
   return (
     <div>
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route path="/quotes" exact>
-            <AllQuote />
-          </Route>
-          <Route path="/quotes/:quoteId">
-            <QuoteDetail />
-          </Route>
-          <Route path="/new-quote">
-            <NewQuote />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
+        <Suspense fallback={<div className="centered"><LoadingSpinner /></div>}>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/quotes" />
+            </Route>
+            <Route path="/quotes" exact>
+              <AllQuotes />
+            </Route>
+            <Route path="/quotes/:quoteId">
+              <QuoteDetail />
+            </Route>
+            <Route path="/new-quote">
+              <NewQuote />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+
+        </Suspense>
       </Layout>
     </div>
   );
