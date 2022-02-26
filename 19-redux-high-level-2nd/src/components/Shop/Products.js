8037ffb47493078ba7d885fc9a460d726c6da172
taeyoonyy <tyyu@lsis.com>
2022-02-26 13:09:29 +0900

    add new project 22-auth

--- 19-redux-high-level-2nd/src/components/Shop/Products.js
+++ 19-redux-high-level-2nd/src/components/Shop/Products.js
@@ -1,16 +1,33 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: "My First book",
+    description: "The first book i ever"
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: "My second book",
+    description: "The second book i ever"
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((item) => <ProductItem
+          id={item.id}
+          key={item.id}
+          title={item.title}
+          price={item.price}
+          description={item.description}
+        />)}
       </ul>
     </section>
   );
---
+++ 22-auth/src/components/Auth/AuthForm.js
@@ -0,0 +1,83 @@
+import { useState, useRef } from "react";
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    // optional: add validation
+
+    if (isLogin) {
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDyZ-p5wvCSv1Xyp-GrBCjLfjQkyhEtWjg",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "applcation/json",
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          console.log("fetch ok!!!");
+        } else {
+          return res.json().then((data) => {
+            // show an error modal
+            let errorMessage = "Authentication failed!";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
+          });
+        }
+      });
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input ref={emailInputRef} type="email" id="email" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            ref={passwordInputRef}
+            type="password"
+            id="password"
+            required
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
